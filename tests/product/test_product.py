9ca6c4a304e78bbfbb5a65492454d21a9274496a
RenatoDourad0 <renatocvdourado@hotmail.com>
2023-04-10 09:05:10 -0300

    req 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        "225",
+        "Batedeira",
+        "Bend",
+        "14/01/1997",
+        "14/01/2027",
+        "3312312445",
+        "n/a",
+    )
+
+    assert product.id == "225"
+    assert product.nome_do_produto == "Batedeira"
+    assert product.nome_da_empresa == "Bend"
+    assert product.data_de_fabricacao == "14/01/1997"
+    assert product.data_de_validade == "14/01/2027"
+    assert product.numero_de_serie == "3312312445"
+    assert product.instrucoes_de_armazenamento == "n/a"
