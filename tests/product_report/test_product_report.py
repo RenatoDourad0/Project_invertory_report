e01102a3e9ec48ef3a2176799188985c1594cdea
RenatoDourad0 <renatocvdourado@hotmail.com>
2023-04-11 08:21:30 -0300

    feat: req 8 - test

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,26 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    def product():
+        p = Product(
+            54323,
+            "Camisa",
+            "RCampos",
+            "2020-10-23",
+            "2022-10-23",
+            "64326473091283",
+            "none",
+        )
+        return p.__repr__()
+
+    def productReturn():
+        return (
+            "O produto Camisa"
+            " fabricado em 2020-10-23"
+            " por RCampos com validade"
+            " até 2022-10-23"
+            " precisa ser armazenado none."
+        )
+
+    assert product() == productReturn()
